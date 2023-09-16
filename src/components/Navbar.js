aa7dc38f8787ed1dc60dab6ee9af8674acd571a0
ruturaj220 <87797944+ruturaj220@users.noreply.github.com>
2023-09-16 21:47:49 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,14 +6,7 @@ const Navbar = () => {
             <nav className="navbar navbar-expand-lg navbar bg-primary">
                 <div className="container-fluid">
                     <a className="navbar-brand" href="/">Full Stack Application</a>
-                    {/*<button className="navbar-toggler"*/}
-                    {/*        type="button" data-bs-toggle="collapse"*/}
-                    {/*        data-bs-target="#navbarSupportedContent"*/}
-                    {/*        aria-controls="navbarSupportedContent"*/}
-                    {/*        aria-expanded="false"*/}
-                    {/*        aria-label="Toggle navigation">*/}
-                    {/*    <span className="navbar-toggler-icon"></span>*/}
-                    {/*</button>*/}
+                   
                     <Link className="btn btn-outline-light" to={"/addUser"}>Add User</Link>
                 </div>
             </nav>
